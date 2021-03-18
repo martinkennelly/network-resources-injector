148e9e8fc8ec9b2fcf373fd91dd4f9c36ca0fc44
Niamh Hennigan <niamh.hennigan@intel.com>
2021-03-18 15:29:21 +0000

    Add test case that verify resouceName in custome namespace

--- test/e2e/httpserver_test.go
+++ test/e2e/httpserver_test.go
@@ -65,7 +65,7 @@ var _ = Describe("Network injection testing", func() {
 	var nad *cniv1.NetworkAttachmentDefinition
 	var err error
 
-	Context("one network request", func() {
+	Context("one network request in default namespace", func() {
 		BeforeEach(func() {
 			nad = util.GetResourceSelectorOnly(testNetworkName, *testNs, testNetworkResName)
 			err = util.ApplyNetworkAttachmentDefinition(networkClient.K8sCniCncfIoV1Interface, nad, timeout)
@@ -97,7 +97,7 @@ var _ = Describe("Network injection testing", func() {
 		})
 	})
 
-	Context("two network requests", func() {
+	Context("two network requests in default namespace", func() {
 		BeforeEach(func() {
 			nad = util.GetResourceSelectorOnly(testNetworkName, *testNs, testNetworkResName)
 			err = util.ApplyNetworkAttachmentDefinition(networkClient.K8sCniCncfIoV1Interface, nad, timeout)
@@ -124,6 +124,43 @@ var _ = Describe("Network injection testing", func() {
 			limNo, ok := pod.Spec.Containers[0].Resources.Requests[testNetworkResName]
 			Expect(ok).Should(BeTrue())
 			Expect(limNo.String()).Should(Equal("2"))
+		})
+	})
+
+	Context("one network request in custom namespace", func() {
+		BeforeEach(func() {
+			testNamespace := "mysterious"
+			err = util.CreateNamespace(cs.CoreV1Interface, testNamespace, timeout)
+			Expect(err).Should(BeNil())
+
+			nad = util.GetResourceSelectorOnly(testNetworkName, testNamespace, testNetworkResName)
+			err = util.ApplyNetworkAttachmentDefinition(networkClient.K8sCniCncfIoV1Interface, nad, timeout)
+			Expect(err).Should(BeNil())
+
+			pod = util.GetOneNetwork(testNetworkName, testNamespace, defaultPodName)
+			err = util.CreateRunningPod(cs.CoreV1Interface, pod, timeout, interval)
+			Expect(err).Should(BeNil())
+			Expect(pod.Name).ShouldNot(BeNil())
+			pod, err = util.UpdatePodInfo(cs.CoreV1Interface, pod, timeout)
+			Expect(err).Should(BeNil())
+		})
+
+		AfterEach(func() {
+			testNamespace := "mysterious"
+			err = util.DeleteNamespace(cs.CoreV1Interface, testNamespace, timeout)
+			Expect(err).Should(BeNil())
+		})
+
+		It("should have one limit injected", func() {
+			limNo, ok := pod.Spec.Containers[0].Resources.Limits[testNetworkResName]
+			Expect(ok).Should(BeTrue())
+			Expect(limNo.String()).Should(Equal("1"))
+		})
+
+		It("should have one request injected", func() {
+			limNo, ok := pod.Spec.Containers[0].Resources.Requests[testNetworkResName]
+			Expect(ok).Should(BeTrue())
+			Expect(limNo.String()).Should(Equal("1"))
 		})
 	})
 })
